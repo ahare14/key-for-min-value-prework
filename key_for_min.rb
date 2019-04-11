ad5e9a7003b6b123eab23d69a80eb62d334820e5
Alex Hare <alexanderhare14@gmail.com>
2019-04-11 17:15:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash == {}
+    return nil
   # Convert hash to array
 name_a = name_hash.to_a
 
