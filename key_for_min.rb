6743cae9d514dfba2589e0bc16cf152777e34ba0
Alex Hare <alexanderhare14@gmail.com>
2019-04-11 16:57:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,14 @@ def key_for_min_value(name_hash)
   if name_hash = {}
     return nil
   else
-    
-
-
+    array = name_hast.to_a
+    value_1 = 0
+    key_1 = 0
+    array.each do |i|
+      if i[1] < value_1
+        value_1 = i[1]
+        key_1 = i[0]
+      end
+    end
+    key_1
 end
