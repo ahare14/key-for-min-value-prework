5605ebe363198a3cb4d78485b17d58a5d43e26c3
Alex Hare <alexanderhare14@gmail.com>
2019-04-11 17:17:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     return nil
   else
     name_a = name_hash.to_a
-    d_value= 1000
+    d_value= 0
     d_key= 0
     name_a.each do |i|
       if i[1] < d_value
