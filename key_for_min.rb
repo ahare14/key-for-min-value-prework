3f8f29a107641e091b8445d57ad210d3c2c2d5ad
Alex Hare <alexanderhare14@gmail.com>
2019-04-11 17:21:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,12 +8,12 @@ def key_for_min_value(name_hash)
     array = name_hash.to_a
     low_value = array[0][1]
     low_key = array[0][0]
-    name_a.each do |i|
-      if i[1] < d_value
-        d_value = i[1]
-        d_key = i[0]
+    array.each do |i|
+      if i[1] < low_value
+        low_value = i[1]
+        low_key = i[0]
       end
     end
-    return d_key
+    return low_key
   end
 end
