0faace2a87a472fb3cb3db7379b046e816f63162
Alex Hare <alexanderhare14@gmail.com>
2019-04-11 17:16:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,23 +4,16 @@
 def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
-  # Convert hash to array
-name_a = name_hash.to_a
-
-# Default key value
-d_value= 1000
-d_key= 0
-
-# Iterate new array
-name_a.each do |i|
-    # If current value is lower than default, change value&key
-    if i[1] < d_value
-      d_value = i[1]
-      d_key = i[0]
+  else
+    name_a = name_hash.to_a
+    d_value= 1000
+    d_key= 0
+    name_a.each do |i|
+      if i[1] < d_value
+        d_value = i[1]
+        d_key = i[0]
+      end
     end
-
- end
-
-return d_key
-
-end
+    return d_key
+  end
+end 
